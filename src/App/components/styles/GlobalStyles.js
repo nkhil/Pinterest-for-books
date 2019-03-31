036a4531f831e793f1299866c75ed6ed78c4aac0
Nikhil Vijayan <nkhilv@gmail.com>
2019-04-01 00:30:49 +0100

    Add paragraph line height globally

--- src/App/components/styles/GlobalStyles.js
+++ src/App/components/styles/GlobalStyles.js
@@ -8,6 +8,10 @@ const GlobalStyle = createGlobalStyle`
   body {
     font-family: -apple-system, BlinkMacSystemFont, "Segoe UI", Roboto, Helvetica, Arial, sans-serif, "Apple Color Emoji", "Segoe UI Emoji", "Segoe UI Symbol";
   }
+
+  p {
+    line-height: 1.5;
+  }
 `
 
 export default GlobalStyle
