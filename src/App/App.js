d1ea9bbe91d5e382741fe1d8aae22a781abbad0e
Nikhil Vijayan <nkhilv@gmail.com>
2019-04-01 00:13:26 +0100

    Add Heading, Comonent, Flex and Container

--- src/App/App.js
+++ src/App/App.js
@@ -1,6 +1,9 @@
 import React, { Component, Fragment } from 'react'
 import GlobalStyles from './components/styles/GlobalStyles'
 import A from './components/elements/A'
+import Container from './components/elements/Container'
+import Flex from './components/elements/Flex'
+import Heading from './components/elements/Heading'
 
 class App extends Component {
   state = {}
@@ -9,11 +12,48 @@ class App extends Component {
     return (
       <Fragment>
         <GlobalStyles />
-        <h1>Hello World</h1>
-        <p>
-          This is{' '}
-          <A href="http://nikhilvijayan.com">a link I'd like to visit.</A>
-        </p>
+        <Container style={{ backgroundColor: '#71CF00' }}>
+          <Flex column contentCenter alignCenter>
+            <h1>Hello World</h1>
+            <p>
+              This is{' '}
+              <A href="http://nikhilvijayan.com">a link I'd like to visit.</A> I
+              want to know what exactly is happening when a long sentence is
+              parsed in What you are about to see is real; the litigants on the
+              screen are not actors. They are genuine citizens who, having filed
+              their claims in a real small claims court, have been persuaded to
+              drop their suits there and have them settled here, in this
+              forum... the People's Court.
+            </p>
+          </Flex>
+        </Container>
+        <div>
+          <Container>
+            <Heading>Container</Heading>
+          </Container>
+          <Container>
+            <div style={{ backgroundColor: '#fff' }}>
+              <Heading h2>Default Container</Heading>
+              <p>
+                What you are about to see is real; the litigants on the screen
+                are not actors. They are genuine citizens who, having filed
+                their claims in a real small claims court, have been persuaded
+                to drop their suits there and have them settled here, in this
+                forum... the People's Court.
+              </p>
+            </div>
+          </Container>
+          <Container fullVertical style={{ backgroundColor: '#008BF8' }}>
+            <div style={{ height: 300, backgroundColor: '#5CB5FA' }}>
+              <Heading h2>Full Vertical Container</Heading>
+            </div>
+          </Container>
+          <Container full small style={{ backgroundColor: '#71CF00' }}>
+            <div style={{ height: 300, backgroundColor: '#D4FDA2' }}>
+              <Heading h2>Full Small Container</Heading>
+            </div>
+          </Container>
+        </div>
       </Fragment>
     )
   }
