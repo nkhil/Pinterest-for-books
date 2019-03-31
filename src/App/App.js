5ee9bc793d1f378910faa9e43f41b33bcc322d61
Nikhil Vijayan <nkhilv@gmail.com>
2019-03-31 02:49:15 +0100

    Add state to App

--- src/App/App.js
+++ src/App/App.js
@@ -1,8 +1,14 @@
 import React, { Component, Fragment } from 'react'
 
 class App extends Component {
+  state = {}
+
   render() {
-    return <h1>Hello World</h1>
+    return (
+      <Fragment>
+        <h1>Hello World</h1>
+      </Fragment>
+    )
   }
 }
 
