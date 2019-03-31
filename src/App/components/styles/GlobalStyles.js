71476c8b9f23f0016eca939bd7dc89d0c602e5e0
Nikhil Vijayan <nkhilv@gmail.com>
2019-04-01 00:29:40 +0100

    Add styledNormalize to GlobalStyles

--- src/App/components/styles/GlobalStyles.js
+++ src/App/components/styles/GlobalStyles.js
@@ -1,6 +1,10 @@
 import { createGlobalStyle } from 'styled-components'
+import styledNormalize from 'styled-normalize'
 
 const GlobalStyle = createGlobalStyle`
+  
+  ${styledNormalize}
+
   body {
     font-family: -apple-system, BlinkMacSystemFont, "Segoe UI", Roboto, Helvetica, Arial, sans-serif, "Apple Color Emoji", "Segoe UI Emoji", "Segoe UI Symbol";
   }
