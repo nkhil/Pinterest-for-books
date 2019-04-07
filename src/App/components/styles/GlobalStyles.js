6ff25cb13395049429aea749b9eda28f1fdff95f
Nikhil Vijayan <nkhilv@gmail.com>
2019-04-07 21:17:38 +0100

    Modify GlobalStyles

--- src/App/components/styles/GlobalStyles.js
+++ src/App/components/styles/GlobalStyles.js
@@ -7,11 +7,24 @@ const GlobalStyle = createGlobalStyle`
 
   body {
     font-family: -apple-system, BlinkMacSystemFont, "Segoe UI", Roboto, Helvetica, Arial, sans-serif, "Apple Color Emoji", "Segoe UI Emoji", "Segoe UI Symbol";
+    background-color: #f9fbff;
   }
 
   p {
     line-height: 1.8;
     font-size: 1.1rem;
+    padding-bottom: 50px;
+  }
+
+  li {
+    list-style-type: none;
+    margin-right: 20px;
+    margin-top: 20px;
+  }
+
+  img {
+    border-radius: 5px;
+    box-shadow: rgba(25, 4, 69, 0.1) 0px 20px 50px;
   }
 `
 
