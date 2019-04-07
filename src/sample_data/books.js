d8395379a21332a13906e33f4c3d3acbf1bf2622
Nikhil Vijayan <nkhilv@gmail.com>
2019-04-07 21:09:54 +0100

    Add sample books

---
+++ src/sample_data/books.js
@@ -0,0 +1,39 @@
+const sampleBooks = {
+  book1: {
+    bookName: 'Book number 1',
+    bookUrl: '/images/1.jpg',
+    desc:
+      'Everyones favorite white fish. We will cut it to the size you need and ship it.',
+    price: 1724,
+  },
+  book2: {
+    bookName: 'Book number 2',
+    bookUrl: '/images/2.jpg',
+    desc:
+      'Everyones favorite white fish. We will cut it to the size you need and ship it.',
+    price: 1724,
+  },
+  book3: {
+    bookName: 'Book number 3',
+    bookUrl: '/images/3.jpg',
+    desc:
+      'Everyones favorite white fish. We will cut it to the size you need and ship it.',
+    price: 1724,
+  },
+  book4: {
+    bookName: 'Book number 4',
+    bookUrl: '/images/4.jpg',
+    desc:
+      'Everyones favorite white fish. We will cut it to the size you need and ship it.',
+    price: 1724,
+  },
+  book5: {
+    bookName: 'Book number 5',
+    bookUrl: '/images/5.jpg',
+    desc:
+      'Everyones favorite white fish. We will cut it to the size you need and ship it.',
+    price: 1724,
+  },
+}
+
+export default sampleBooks
